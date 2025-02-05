cc04ceda84f5a713ffdb975d772d07fdbb2ca6f0
nikrenxd <nikrenc1101@gmail.com>
2025-02-05 19:08:30 +0200

    created models for cards app

--- src/apps/cards/models.py
+++ src/apps/cards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from src.common.models import TimeStampedModel
+
+
+class Card(TimeStampedModel):
+    question = models.TextField(max_length=325)
+    answer = models.TextField(max_length=325)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    deck = models.ForeignKey("decks.Deck", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "card"
+        verbose_name_plural = "cards"
