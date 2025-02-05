9a8e0d881259cc4ee8634653bb98bd1c05daa6d3
nikrenxd <nikrenc1101@gmail.com>
2025-02-05 15:06:25 +0200

    model for decks app

--- src/apps/decks/models.py
+++ src/apps/decks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from src.common.models import TimeStampedModel
 
-# Create your models here.
+
+class Deck(TimeStampedModel):
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True, max_length=255)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        to="users.User", on_delete=models.CASCADE, related_name="decks"
+    )
+
+    class Meta:
+        verbose_name = "deck"
+        verbose_name_plural = "decks"
