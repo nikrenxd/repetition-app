54539c0f49e7c2f0d9b0e94d9630a8baf4303c7e
nikrenxd <nikrenb1101@gmail.com>
2025-02-12 20:44:57 +0000

    card state for model Card

--- src/apps/cards/models.py
+++ src/apps/cards/models.py
@@ -6,10 +6,25 @@ from src.common.models import TimeStampedModel
 class Card(TimeStampedModel):
     question = models.TextField(max_length=325)
     answer = models.TextField(max_length=325)
-    answered = models.BooleanField(default=False)
-    user = models.ForeignKey("users.User", related_name="cards", on_delete=models.CASCADE)
-    deck = models.ForeignKey("decks.Deck", related_name="cards", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        "users.User", related_name="cards", on_delete=models.CASCADE
+    )
+    deck = models.ForeignKey(
+        "decks.Deck", related_name="cards", on_delete=models.CASCADE
+    )
 
     class Meta:
         verbose_name = "card"
         verbose_name_plural = "cards"
+
+
+class CardState(models.Model):
+    answered = models.BooleanField(default=False)
+    card = models.OneToOneField(
+        Card,
+        related_name="card_state",
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = "card_state"
