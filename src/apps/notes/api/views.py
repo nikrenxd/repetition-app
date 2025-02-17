310da9b511cfee7a6e8bfa8cda18aaf623f7abdb
nikrenxd <nikrenc1101@gmail.com>
2025-02-17 21:12:46 +0200

    mixin for checking object owner to prevent creating notes with not owned cards

--- src/apps/notes/api/views.py
+++ src/apps/notes/api/views.py
@@ -1,12 +1,14 @@
 from django.db.models import QuerySet
-from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
+from src.apps.cards.models import Card
 from src.apps.notes.api.serializers import NoteSerializer
 from src.apps.notes.models import Note
+from src.common.mixins import OwnObjectMixin
 
 
-class NotesViewSet(viewsets.ModelViewSet):
+class NotesViewSet(OwnObjectMixin, ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = (IsAuthenticated,)
@@ -17,4 +19,9 @@ class NotesViewSet(viewsets.ModelViewSet):
         return qs.filter(user=self.request.user)
 
     def perform_create(self, serializer):
+        self.own_object("card", Card)
         serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        self.own_object("card", Card)
+        super().perform_update(serializer)
