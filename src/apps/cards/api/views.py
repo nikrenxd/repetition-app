31a19a438ed6e152347e8f016756f27b3e613a2c
nikrenxd <nikrenc1101@gmail.com>
2025-02-17 20:43:55 +0200

    mixin for check object owner to prevent creating cards with not owned decks

--- src/apps/cards/api/views.py
+++ src/apps/cards/api/views.py
@@ -4,23 +4,30 @@ from rest_framework.permissions import IsAuthenticated
 
 from src.apps.cards.api.serializers import CardSerializer, CardCreateSerializer
 from src.apps.cards.models import Card
+from src.apps.decks.models import Deck
+from src.common.mixins import OwnObjectMixin
 
 
-class CardViewSet(ModelViewSet):
+class CardViewSet(ModelViewSet, OwnObjectMixin):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        qs: QuerySet  = super().get_queryset()
+        qs: QuerySet = super().get_queryset()
 
         return qs.filter(user=self.request.user)
-    
+
     def get_serializer_class(self):
         if self.action == "create" or self.action == "partial_update":
             return CardCreateSerializer
-        
+
         return super().get_serializer_class()
-    
+
     def perform_create(self, serializer):
+        self.own_object("deck", Deck)
         serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        self.own_object("deck", Deck)
+        super().perform_update(serializer)
--- src/common/mixins.py
+++ src/common/mixins.py
@@ -1,21 +1,12 @@
+from django.db.models import Model
+from rest_framework.exceptions import NotFound
 from rest_framework.mixins import (
     RetrieveModelMixin,
     UpdateModelMixin,
-    DestroyModelMixin,
     CreateModelMixin,
 )
 
-
-class UpdateDestroyMixin(UpdateModelMixin, DestroyModelMixin):
-    pass
-
-
-class RetrieveUpdateDestroyMixin(
-    RetrieveModelMixin,
-    UpdateModelMixin,
-    DestroyModelMixin,
-):
-    pass
+from src.common.services import user_owns_object
 
 
 class CreateRetrieveUpdateMixin(
@@ -24,3 +15,12 @@ class CreateRetrieveUpdateMixin(
     UpdateModelMixin,
 ):
     pass
+
+
+class OwnObjectMixin:
+    def own_object(self, obj_name: str, model: Model):
+        """Utilize user_owns_object to check if a current user owns a given object"""
+        is_own = user_owns_object(obj_name, model, self.request)
+
+        if not is_own:
+            raise NotFound
