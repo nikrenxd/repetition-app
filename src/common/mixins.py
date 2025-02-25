5f6285f6b16b9420f1de2fa47dc73da2e250521c
nikrenxd <nikrenc1101@gmail.com>
2025-02-25 15:29:12 +0200

    removed OwnObjectMixin and replace it with permission instead

--- src/common/mixins.py
+++ src/common/mixins.py
@@ -1,12 +1,8 @@
-from django.db.models import Model
-from rest_framework.exceptions import NotFound
 from rest_framework.mixins import (
     RetrieveModelMixin,
     UpdateModelMixin,
     CreateModelMixin,
 )
-
-from src.common.services import user_owns_object
 
 
 class CreateRetrieveUpdateMixin(
@@ -15,12 +11,3 @@ class CreateRetrieveUpdateMixin(
     UpdateModelMixin,
 ):
     pass
-
-
-class OwnObjectMixin:
-    def own_object(self, obj_name: str, model: Model):
-        """Utilize user_owns_object to check if a current user owns a given object"""
-        is_own = user_owns_object(obj_name, model, self.request)
-
-        if not is_own:
-            raise NotFound
