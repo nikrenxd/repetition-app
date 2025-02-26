868c6806d652514f0d1fa506aec27ed4c2bed940
nikrenxd <nikrenc1101@gmail.com>
2025-02-26 20:18:11 +0200

    created serializer for CardState

--- src/apps/cards/api/serializers.py
+++ src/apps/cards/api/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
 
-from src.apps.cards.models import Card
+from src.apps.cards.models import Card, CardState
 
 
 class CardSerializer(serializers.ModelSerializer):
+    answered = serializers.BooleanField(source="card_state.answered")
+
     class Meta:
         model = Card
-        fields = ("id", "question", "answer")
+        fields = ("id", "question", "answer", "answered")
 
 
 class CardCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = ("question", "answer", "deck")
+
+
+class CardStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CardState
+        fields = ("answered",)
