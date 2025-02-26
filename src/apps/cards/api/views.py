ef81a828ca0859c54becdb4c501772db691f9086
nikrenxd <nikrenc1101@gmail.com>
2025-02-26 20:11:40 +0200

    endpoint for changing card state

--- src/apps/cards/api/views.py
+++ src/apps/cards/api/views.py
@@ -1,10 +1,19 @@
 from django.db.models import QuerySet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
-from src.apps.cards.api.serializers import CardSerializer, CardCreateSerializer
+from src.apps.cards.api.serializers import (
+    CardSerializer,
+    CardCreateUpdateSerializer,
+    CardStateSerializer,
+)
 from src.apps.cards.models import Card
 from src.apps.cards.permissions import CardCurrentUserOwnDeckPermission
+from src.apps.cards.services import CardService
 
 
 class CardViewSet(ModelViewSet):
@@ -19,9 +28,22 @@ class CardViewSet(ModelViewSet):
 
     def get_serializer_class(self):
         if self.action == "create" or self.action == "partial_update":
-            return CardCreateSerializer
+            return CardCreateUpdateSerializer
+        elif self.action == "update_card_state":
+            return CardStateSerializer
 
         return super().get_serializer_class()
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        card = serializer.save(user=self.request.user)
+        CardService.create_card_state(card)
+
+    @action(detail=True, methods=["patch"], url_path="state")
+    def update_card_state(self, request: Request, pk: int = None):
+        card = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        CardService.update_card_state(card, serializer.validated_data, pk)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
