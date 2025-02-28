9d5ebd0985dc2e9d2635b67e76189c6fba79bb76
nikrenxd <nikrenc1101@gmail.com>
2025-02-28 15:55:16 +0200

    add methods check to permission

--- src/common/permissions.py
+++ src/common/permissions.py
@@ -1,19 +1,20 @@
 from rest_framework import permissions
 from rest_framework.generics import get_object_or_404
 
+CHECK_METHODS = ["POST", "PATCH", "PUT"]
+
 
 class CurrentUserOwnObjectPermission(permissions.BasePermission):
     obj_name_in_request = None
     model = None
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
+        """Check if user owns a related object"""
+
+        if request.method not in CHECK_METHODS:
             return True
 
         obj_id = request.data.get(self.obj_name_in_request, None)
-
-        if not obj_id:
-            return False
 
         obj = get_object_or_404(self.model, pk=obj_id)
 
