6264d0d5294b585d33ef5c5f1b35842428be2ac7
nikrenxd <nikrenc1101@gmail.com>
2025-02-28 17:27:36 +0200

    nested field for notes inside CardSerializer

--- src/apps/cards/api/serializers.py
+++ src/apps/cards/api/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 
 from src.apps.cards.models import Card, CardState
+from src.apps.notes.api.serializers import NoteSerializer
 
 
 class CardSerializer(serializers.ModelSerializer):
+    notes = NoteSerializer(many=True)
     answered = serializers.BooleanField(source="card_state.answered")
 
     class Meta:
         model = Card
-        fields = ("id", "question", "answer", "answered")
+        fields = ("id", "question", "answer", "notes", "answered")
 
 
 class CardCreateUpdateSerializer(serializers.ModelSerializer):
