c61a13d5b478041ea546e015c2f92446a1bf7560
nikrenxd <nikrenc1101@gmail.com>
2025-03-08 14:02:14 +0200

    created filters for decks

--- src/apps/decks/api/views.py
+++ src/apps/decks/api/views.py
@@ -1,5 +1,6 @@
 from django.db.models import QuerySet, Count
 from rest_framework import viewsets, permissions
+from django_filters import rest_framework as filters
 
 from src.apps.decks.api.serializers import (
     DeckSerializer,
@@ -9,10 +10,22 @@ from src.apps.decks.api.serializers import (
 from src.apps.decks.models import Deck
 
 
+class DeckFilter(filters.FilterSet):
+    completed = filters.BooleanFilter()
+
+    class Meta:
+        model = Deck
+        fields = {
+            "name": ["icontains"],
+        }
+
+
 class DeckViewSet(viewsets.ModelViewSet):
     queryset = Deck.objects.all()
     serializer_class = DeckSerializer
     permission_classes = (permissions.IsAuthenticated,)
+    filterset_class = DeckFilter
+    ordering_fields = ["name", "created_at"]
     http_method_names = ["get", "post", "delete", "patch"]
 
     def get_queryset(self):
