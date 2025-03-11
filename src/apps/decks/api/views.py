996dceb2d8e1ded2f7a0876f6016d69ec5b137ba
nikrenxd <nikrenc1101@gmail.com>
2025-03-11 13:18:11 +0200

    created two new endpoints: start_deck, end_deck

--- src/apps/decks/api/views.py
+++ src/apps/decks/api/views.py
@@ -1,13 +1,25 @@
-from django.db.models import QuerySet, Count
-from rest_framework import viewsets, permissions
-from django_filters import rest_framework as filters
+import logging
 
+from django.db.models import QuerySet, Count, Prefetch
+from django.http import HttpResponseRedirect
+from rest_framework import viewsets, permissions, status
+from django_filters import rest_framework as filters
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from src.apps.cards.models import Card
 from src.apps.decks.api.serializers import (
     DeckSerializer,
     DeckCreateUpdateSerializer,
     DeckRetrieveSerializer,
+    DeckEndSerializer,
 )
 from src.apps.decks.models import Deck
+from src.apps.decks.services import DeckServices
+
+logger = logging.getLogger(__name__)
 
 
 class DeckFilter(filters.FilterSet):
@@ -29,12 +41,27 @@ class DeckViewSet(viewsets.ModelViewSet):
     http_method_names = ["get", "post", "delete", "patch"]
 
     def get_queryset(self):
-        qs: QuerySet = super().get_queryset().filter(user=self.request.user)
+        qs: QuerySet[Deck] = super().get_queryset().filter(user=self.request.user)
+        fetch_cards_qs: QuerySet[Card] = Card.objects.select_related(
+            "card_state"
+        ).prefetch_related("notes")
 
         if self.action == "list":
             return qs.annotate(cards_num=Count("cards"))
         if self.action == "retrieve":
-            return qs.prefetch_related("cards__card_state", "cards__notes")
+            return qs.prefetch_related(
+                Prefetch(
+                    "cards",
+                    queryset=fetch_cards_qs,
+                )
+            )
+        if self.action == "start_deck":
+            return qs.prefetch_related(
+                Prefetch(
+                    "cards",
+                    queryset=fetch_cards_qs.filter(card_state__answered=False),
+                )
+            )
 
         return qs
 
@@ -48,3 +75,29 @@ class DeckViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(
+        detail=True,
+        methods=["get"],
+        serializer_class=DeckRetrieveSerializer,
+    )
+    def start_deck(self, request: Request, pk=None):
+        deck = self.get_object()
+        serializer = self.get_serializer(deck)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(
+        detail=True,
+        methods=["post"],
+        serializer_class=DeckEndSerializer,
+    )
+    def end_deck(self, request: Request, pk=None):
+        deck = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        if serializer.validated_data["complete"]:
+            DeckServices.deck_completed(deck)
+
+        return HttpResponseRedirect(redirect_to=reverse("decks-list"))
