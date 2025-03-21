25127fc7ab484aa7bc0072366df64970d32931de
nikrenxd <nikrenc1101@gmail.com>
2025-03-21 18:10:29 +0200

    created test for /users/me endpoint

--- tests/users/test_api.py
+++ tests/users/test_api.py
@@ -4,21 +4,26 @@ from rest_framework.reverse import reverse
 
 @pytest.mark.django_db
 class TestUserAPI:
-    def test_create_api(self, api_client, user_credentials):
+    def test_users_create(self, api_client, user_credentials):
         response = api_client.post(reverse("users-list"), data=user_credentials)
 
         assert response.status_code == 201
 
-    def test_retrieve_api(self, authenticated_client):
+    def test_users_retrieve(self, authenticated_client):
         response = authenticated_client.get(reverse("users-detail", kwargs={"pk": 1}))
 
         assert response.status_code == 200
 
-    def test_partial_update_api(self, authenticated_client):
+    def test_users_partial_update(self, authenticated_client):
         data = {"email": "newmail@mail.com"}
         response = authenticated_client.patch(
             reverse("users-detail", kwargs={"pk": 1}),
             data=data,
         )
+
+        assert response.status_code == 200
+
+    def test_users_me(self, authenticated_client):
+        response = authenticated_client.get(reverse("users-me"))
 
         assert response.status_code == 200
