ea5835d0e9dae11bd553aea214e9262a9a7c3c24
nikrenxd <nikrenb1101@gmail.com>
2025-04-03 12:10:30 +0000

    created test for get_user_statistic method

--- tests/users/test_services.py
+++ tests/users/test_services.py
@@ -18,3 +18,11 @@ def test_create_user_statistic(user):
     user_stats = UserService.create_user_statistic(user)
 
     assert user_stats.user == user
+
+
+@pytest.mark.django_db
+def test_get_user_statistic():
+    qs = User.objects.filter(id=1)
+    user_stats = UserService.get_user_statistic(qs)
+
+    assert user_stats.total_decks >= 0
