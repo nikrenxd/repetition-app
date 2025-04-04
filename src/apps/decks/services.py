549448ef00d23255ab964823a74f7042f7801295
nikrenxd <nikrenb1101@gmail.com>
2025-04-04 09:26:00 +0000

    added logging and exception handling in decks services

--- src/apps/decks/services.py
+++ src/apps/decks/services.py
@@ -1,8 +1,13 @@
+import logging
+from django.db import DatabaseError
 from django.db.models import QuerySet, Count, Q
 
 
 from src.apps.decks.models import Deck
 from src.apps.users.models import UserStatistic
+
+
+logger = logging.getLogger(__name__)
 
 
 class DeckServices:
@@ -15,23 +20,27 @@ class DeckServices:
 
     @staticmethod
     def deck_completed(deck: Deck) -> bool | None:
-        not_answered_cards_in_deck = deck.cards.filter(
-            card_state__answered=False
-        ).count()
+        try:
+            not_answered_cards_in_deck = deck.cards.filter(
+                card_state__answered=False
+            ).count()
 
-        if not_answered_cards_in_deck == 0:
-            DeckServices.deck_change_completed(deck, True)
-            return True
-        elif not_answered_cards_in_deck != 0 and deck.completed is True:
-            DeckServices.deck_change_completed(deck, False)
+            if not_answered_cards_in_deck == 0:
+                DeckServices.deck_change_completed(deck, True)
+                return True
+            elif not_answered_cards_in_deck != 0 and deck.completed is True:
+                DeckServices.deck_change_completed(deck, False)
+        except DatabaseError as e:
+            logger.error(f"Error when updating deck completion state {e}")
 
     @staticmethod
     def deck_update_decks_statistic(qs: QuerySet[Deck], user_id: int):
-        decks_stats = qs.aggregate(
-            total_decks=Count("id"),
-            completed_decks=Count("id", filter=Q(completed=True)),
-        )
+        try:
+            decks_stats = qs.aggregate(
+                total_decks=Count("id"),
+                completed_decks=Count("id", filter=Q(completed=True)),
+            )
 
-        stats = UserStatistic.objects.filter(user_id=user_id).update(**decks_stats)
-
-        return stats
+            UserStatistic.objects.filter(user_id=user_id).update(**decks_stats)
+        except DatabaseError as e:
+            logger.error(f"Error when updating deck statistic {e}")
